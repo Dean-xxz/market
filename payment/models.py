f912ad9567788fb95c5f85609db59185edb41333
Dean-xxz <1220543004@qq.com>
2018-05-29 17:24:07 +0800

    create payment and product model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+
+"""
+此模块定义商城订单模型
+"""
 
 # Create your models here.
+import json
+from django.db import models
+from utils.basemodel.base import BaseModel
+from django.core import serializers
+from product.models import Product
+from accounts.models import User_profile
+
+
+class Order(BaseModel):
+
+    """
+    支付系统订单表，外键关联产品表Product
+    """
+    CHANNEL_CHOICES = (
+            ('Z',("支付宝支付")),
+            ('W',("微信支付")),
+        )
+
+    user = models.ForeignKey('accounts.User_profile',verbose_name = "用户",related_name = "order_user",null=True,blank=True)
+    product = models.ForeignKey("product.Product",verbose_name = "商品",related_name = "order_product",null=True,blank=True)
+    total_fee = models.DecimalField(max_digits = 10,decimal_places = 2,verbose_name="总价")
+    channel = models.CharField(max_length = 1,verbose_name = "支付渠道",null = True,blank = True,choices = CHANNEL_CHOICES)
+    is_payment = models.BooleanField(verbose_name="是否付款",default=False)
+    is_delivery =models.BooleanField(verbose_name="是否发货",default=False)
+    is_refound = models.BooleanField(verbose_name="是否退款",default=False)
+    is_received = models.BooleanField(verbose_name="是否赠送",default=False)
+    remarks = models.CharField(max_length=1024,verbose_name="备注",null=True,blank=True)
+
+
+    class Meta:
+        verbose_name = "订单"
+        verbose_name_plural = "订单"
+        ordering = ["-update_time",]
+
+    # def __str__(self):
+    #     return self.product
+
+    def get_json(self):
+        serials = serializers.serialize("json", [self])
+        struct = json.loads(serials)
+        data = struct[0]['fields']
+        if 'pk' in struct[0]:
+            data['id'] = struct[0]['pk']
+        return data
--- product/models.py
+++ product/models.py
@@ -1,3 +1,78 @@
+"""
+此处定义商品模块模型：
+    商品分类表
+    商品表
+    一对多关系
+"""
+
+import json
 from django.db import models
+from utils.basemodel.base import BaseModel
+from django.core import serializers
 
 # Create your models here.
+class Category(BaseModel):
+
+    """
+    商品分类表：定义商品分类，eg：水果；奶茶；家具……
+    """
+    title = models.CharField(max_length=128, verbose_name="商品分类标题")
+    descp = models.CharField(max_length=1024,verbose_name="分类描述",null=True,blank=True)
+    order = models.PositiveSmallIntegerField(verbose_name="分类排序",default=0,help_text="在列表中的顺序")
+
+
+    class Meta:
+        verbose_name = "商品分类"
+        verbose_name_plural = "商品分类"
+        ordering = ['order',]
+
+    def __str__(self):
+        return self.title
+
+    def get_json(self):
+        serials = serializers.serialize("json", [self])
+        struct = json.loads(serials)
+        data = struct[0]['fields']
+        if 'pk' in struct[0]:
+            data['id'] = struct[0]['pk']
+        return data
+
+
+
+class Product(BaseModel):
+
+    """
+    商品表：定义商品详细属性
+    """
+
+    category = models.ForeignKey("Category",verbose_name="所属商品分类",help_text="请选择该商品所属分类")
+    title = models.CharField(max_length=128, verbose_name="商品标题")
+    english_title = models.CharField(max_length=128,verbose_name="英文标题",null=True,blank=True)
+    pre_info = models.CharField(max_length=1024,verbose_name="优惠信息",null=True,blank=True)
+    descp = models.TextField(max_length=1024,verbose_name="商品简介",null=True,blank=True)
+    detail_info = models.TextField(max_length=1024,verbose_name="商品详情")
+    image = models.ImageField(upload_to="media/product/product/image/",verbose_name="商品封面大图")
+    detail_image = models.ImageField(upload_to="media/product/product/detail_image/",verbose_name="商品内部小图")
+    price = models.DecimalField(max_digits=10 ,decimal_places =0,verbose_name="商品价格")
+    pre_price = models.DecimalField(max_digits=10,decimal_places=0,verbose_name="商品优惠价格")
+    count = models.IntegerField(verbose_name="商品库存",default=1)
+    is_hot = models.BooleanField(verbose_name="标记为热销商品",default=False)
+    order = models.PositiveSmallIntegerField(verbose_name="商品顺序",default=0,help_text="该商品在商品列表中的顺序")
+    remarks = models.TextField(verbose_name="备注",null=True,blank=True)
+
+
+    class Meta:
+        verbose_name = "商品表表"
+        verbose_name_plural = "商品表"
+        ordering = ['order',]
+
+    def __str__(self):
+        return self.title
+
+    def get_json(self):
+        serials = serializers.serialize("json", [self])
+        struct = json.loads(serials)
+        data = struct[0]['fields']
+        if 'pk' in struct[0]:
+            data['id'] = struct[0]['pk']
+        return data
